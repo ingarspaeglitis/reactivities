e210ded4a208b6f23b47b4d56717824acef22c70
ingarspaeglitis <ingars@standardsdigital.com>
2026-01-02 20:20:50 +0200

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Queries;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,6 +12,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddMediatR(x =>
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.LicenseKey = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ikx1Y2t5UGVubnlTb2Z0d2FyZUxpY2Vuc2VLZXkvYmJiMTNhY2I1OTkwNGQ4OWI0Y2IxYzg1ZjA4OGNjZjkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2x1Y2t5cGVubnlzb2Z0d2FyZS5jb20iLCJhdWQiOiJMdWNreVBlbm55U29mdHdhcmUiLCJleHAiOiIxNzk4ODQ4MDAwIiwiaWF0IjoiMTc2NzM3NDAxMCIsImFjY291bnRfaWQiOiIwMTliN2ZiMjU1YzA3MTIxOTdmZTk1MDllNWYzODU4OSIsImN1c3RvbWVyX2lkIjoiY3RtXzAxa2R6djZlMTY5OTZuazE3YTg1MGhubmNyIiwic3ViX2lkIjoiLSIsImVkaXRpb24iOiIwIiwidHlwZSI6IjIifQ.FJiQpQmcZpQBJT6QQPqKANAvMcjg6vsiRt7bf4vhvNxfyAiK_JaJYoGlHnqwIFFFa1oPHMdqBlut-WPnH2azFDGtLK0DXhgHLnPAS2B4R-235ZRaq_iHs-oqiI7h1sBPEhNXEW5WjozC6a3Pn5T6SuMu2UaA4e0pTHhHRr9_n-prTgn3nSFopHA7VDPbdCK6Dq-PJJX7tAVHvgL4qecYjDNPW5-96tVNt2zEN7TCqibSHx5cvZeLdKC7KFl3F6mW7H8CIc-8nGAcK6RMKow7khPrtJIjkWwJKjQ9G91lhKh60HAFKnCPfW_Y5TKaGn7ez7w1-SPkDKbFqccUlyZSlA";
+});
 
 var app = builder.Build();
 
